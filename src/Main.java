b06b78d86734a560bf8c2f4591b6c5466d5c0f16
Sam Smith <sam@example.com>
2019-04-27 11:11:19 +0400

    init

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import window.Game;
+
+public class Main {
+    public static void main(String... args) {
+        Game game = new Game("РПГ игра",800,600);
+        game.start();
+    }
+}
---
+++ src/window/Game.java
@@ -0,0 +1,110 @@
+package window;
+
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+
+public class Game implements Runnable {
+
+	private Display display;
+	
+	public int width, height;
+	public String title;
+	
+	private Thread thread;
+	
+	private BufferStrategy bs;
+	private Graphics g;
+	
+	private boolean running = false;
+
+	
+	public Game(String title, int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.title = title;
+	}
+
+	//!!!!!Инициализация
+	private void init() {
+		display = new Display(this.title, width, height);
+	}
+
+	//!!!ОБНОВЛЕНИЕ
+	private void move() {
+
+
+	}
+
+	//!!ПРОРИСОВКА
+	private void render() {
+		bs = display.getCanvas().getBufferStrategy();
+		if (bs == null) {
+			display.getCanvas().createBufferStrategy(3);
+			return;
+		}
+		g = bs.getDrawGraphics();
+		
+		//Рисуем!!!
+		///////////////////////////////////////////////////////////////
+		g.clearRect(0, 0, width, height);
+
+		g.drawRect(10,0,50,50);
+
+
+
+		///////////////////////////////////////////////////////////////
+
+
+		//end draw
+		bs.show();
+		g.dispose();
+	}
+	
+	@Override
+	public void run() {
+		
+		init(); //инициализация
+		
+		int fps = 60;
+		double kolNS = 1000000000 / fps;
+		double delta = 0;
+		long now;
+		long lastTime = System.nanoTime();
+
+		//бесконечный цикл пока running
+		while (running) {
+			now = System.nanoTime();
+
+			delta += (now - lastTime) / kolNS;
+			lastTime = now;
+			
+			if (delta >= 1) {
+				System.out.println("60fps");
+				move();  //пересчет объектов
+				render();  //перерисовка объектов
+				delta--;
+			}
+		}
+		
+		stop();
+	}
+	
+	public synchronized void start() {
+		if (running) return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		if (!running) return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
